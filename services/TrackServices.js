ba15b85ad3027942e11bbe07a664a9be4090e8be
ahmed-tarek <c.tarek2015@gmail.com>
2026-09-20 18:41:11 +0300

    Add TrackServices module for managing tracks

---
+++ services/TrackServices.js
@@ -0,0 +1,37 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/tracks`;
+
+const getTracks = async () => {
+  const response = await fetch(BASE_URL);
+  return response.json();
+};
+
+const createTrack = async (trackData) => {
+  const response = await fetch(BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(trackData),
+  });
+  return response.json();
+};
+
+const updateTrack = async (trackId, trackData) => {
+  const response = await fetch(`${BASE_URL}/${trackId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(trackData),
+  });
+  return response.json();
+};
+
+const deleteTrack = async (trackId) => {
+  const response = await fetch(`${BASE_URL}/${trackId}`, {
+    method: "DELETE",
+  });
+  return response.json();
+};
+
+export default { getTracks, createTrack, updateTrack, deleteTrack };
